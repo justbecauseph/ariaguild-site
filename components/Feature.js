abec00d1c052834addf0d0187b8db9f183373647
sindykatto <hi@sdkatto.com>
2022-03-02 00:40:55 +0800

    fix: changed class to className on component files

--- components/Feature.js
+++ components/Feature.js
@@ -1,27 +1,27 @@
 export default function Feature() {
   return (
     <>
-      <div class="py-12 bg-white">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="lg:text-center">
-            <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+      <div className="py-12 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
               Projects
             </h2>
-            <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               Projects
             </p>
-            <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
               Current projects that we maintain.
             </p>
           </div>
 
-          <div class="mt-10">
-            <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-              <div class="relative">
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <div className="relative">
                 <dt>
-                  <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -29,29 +29,29 @@ export default function Feature() {
                       aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
                       />
                     </svg>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
                     Aria Academy
                   </p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-gray-500">
+                <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Maiores impedit perferendis suscipit eaque, iste dolor
                   cupiditate blanditiis ratione.
                 </dd>
               </div>
 
-              <div class="relative">
+              <div className="relative">
                 <dt>
-                  <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
@@ -59,18 +59,18 @@ export default function Feature() {
                       aria-hidden="true"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
                       />
                     </svg>
                   </div>
-                  <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
                     Conan Exiles Private Server
                   </p>
                 </dt>
-                <dd class="mt-2 ml-16 text-base text-gray-500">
+                <dd className="mt-2 ml-16 text-base text-gray-500">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                   Maiores impedit perferendis suscipit eaque, iste dolor
                   cupiditate blanditiis ratione.
--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <>
-      <div class="relative bg-white overflow-hidden">
-        <div class="max-w-7xl mx-auto">
-          <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <div className="relative bg-white overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg
-              class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
               fill="currentColor"
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
@@ -15,29 +15,29 @@ export default function Hero() {
             </svg>
 
             <div>
-              <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav
-                  class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                  className="relative flex items-center justify-between sm:h-10 lg:justify-start"
                   aria-label="Global"
                 >
-                  <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                    <div class="flex items-center justify-between w-full md:w-auto">
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
                       <a href="#">
-                        <span class="sr-only">Workflow</span>
+                        <span className="sr-only">Workflow</span>
                         <img
-                          class="h-8 w-auto sm:h-10"
+                          className="h-8 w-auto sm:h-10"
                           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                         />
                       </a>
-                      <div class="-mr-2 flex items-center md:hidden">
+                      <div className="-mr-2 flex items-center md:hidden">
                         <button
                           type="button"
-                          class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                          className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                           aria-expanded="false"
                         >
-                          <span class="sr-only">Open main menu</span>
+                          <span className="sr-only">Open main menu</span>
                           <svg
-                            class="h-6 w-6"
+                            className="h-6 w-6"
                             xmlns="http://www.w3.org/2000/svg"
                             fill="none"
                             viewBox="0 0 24 24"
@@ -45,9 +45,9 @@ export default function Hero() {
                             aria-hidden="true"
                           >
                             <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="2"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
                               d="M4 6h16M4 12h16M4 18h16"
                             />
                           </svg>
@@ -55,69 +55,69 @@ export default function Hero() {
                       </div>
                     </div>
                   </div>
-                  <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
                     <a
                       href="#"
-                      class="font-medium text-gray-500 hover:text-gray-900"
+                      className="font-medium text-gray-500 hover:text-gray-900"
                     >
                       About
                     </a>
 
                     <a
                       href="#"
-                      class="font-medium text-gray-500 hover:text-gray-900"
+                      className="font-medium text-gray-500 hover:text-gray-900"
                     >
                       Projects
                     </a>
 
                     <a
                       href="#"
-                      class="font-medium text-gray-500 hover:text-gray-900"
+                      className="font-medium text-gray-500 hover:text-gray-900"
                     >
                       Shop
                     </a>
 
                     <a
                       href="#"
-                      class="font-medium text-gray-500 hover:text-gray-900"
+                      className="font-medium text-gray-500 hover:text-gray-900"
                     >
                       Members
                     </a>
 
                     <a
                       href="#"
-                      class="font-medium text-gray-500 hover:text-gray-900"
+                      className="font-medium text-gray-500 hover:text-gray-900"
                     >
                       Gallery
                     </a>
 
                     <a
                       href="#"
-                      class="font-medium text-indigo-600 hover:text-indigo-500"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
                     >
                       Join
                     </a>
                   </div>
                 </nav>
               </div>
-              <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-                <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div class="px-5 pt-4 flex items-center justify-between">
+              <div className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
                     <div>
                       <img
-                        class="h-8 w-auto"
+                        className="h-8 w-auto"
                         src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                         alt=""
                       />
                     </div>
-                    <div class="-mr-2">
+                    <div className="-mr-2">
                       <button
                         type="button"
-                        class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                        className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                       >
-                        <span class="sr-only">Close main menu</span>
+                        <span className="sr-only">Close main menu</span>
                         <svg
-                          class="h-6 w-6"
+                          className="h-6 w-6"
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
@@ -125,47 +125,47 @@ export default function Hero() {
                           aria-hidden="true"
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
                             d="M6 18L18 6M6 6l12 12"
                           />
                         </svg>
                       </button>
                     </div>
                   </div>
-                  <div class="px-2 pt-2 pb-3 space-y-1">
+                  <div className="px-2 pt-2 pb-3 space-y-1">
                     <a
                       href="#"
-                      class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       About
                     </a>
 
                     <a
                       href="#"
-                      class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       Projects
                     </a>
 
                     <a
                       href="#"
-                      class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       Merch
                     </a>
 
                     <a
                       href="#"
-                      class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                     >
                       Company
                     </a>
                   </div>
                   <a
                     href="#"
-                    class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
                   >
                     {" "}
                     Log in{" "}
@@ -174,18 +174,18 @@ export default function Hero() {
               </div>
             </div>
 
-            <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div class="sm:text-center lg:text-left">
-                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span class="block xl:inline">
+            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">
                     Aria is a{" "}
-                    <span class="block text-indigo-600 xl:inline">
+                    <span className="block text-indigo-600 xl:inline">
                       competitive gaming guild
                     </span>{" "}
                     founded in 2016.
                   </span>
                 </h1>
-                <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                   We pride ourselves in our ability to bring people from
                   different backgrounds together to enjoy games and have fun.
                 </p>
@@ -193,9 +193,9 @@ export default function Hero() {
             </main>
           </div>
         </div>
-        <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
           <img
-            class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
             src="http://placekitten.com/2850"
             alt=""
           />
